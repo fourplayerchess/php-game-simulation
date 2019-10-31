4422c0ccdf6b1b2fc477ee8792944c864cd9a78f
instarto <57154160+instarto@users.noreply.github.com>
2019-10-31 03:42:10 -0400

    Add class.

---
+++ src/Game.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+/**
+ * PHP Game Simulation - Simulate 4 player chess games through PHP code.
+ *
+ * @license (https://www.fsf.org/) GNU General Public License v3.0.
+ *          (https://github.com/fourplayerchess/php-game-simulation/blob/master/LICENSE)
+ *
+ * Permissions of this strong copyleft license are conditioned on making available complete source code
+ * of licensed works and modifications, which include larger works using a licensed work, under the same license.
+ * Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.
+ *
+ * @package fourplayerchess/php-game-simulation.
+ */
+ 
+namespace FourPlayerChess;
+
+/**
+ * The actual game controller.
+ */
+class Game
+{
+}
